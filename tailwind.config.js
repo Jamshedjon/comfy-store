661aafe5fbdccaa661d380297c97a3143f915322
Jamshedjon Hasanjonov <136963308+Jamshedjon@users.noreply.github.com>
2024-12-28 14:24:54 +0500

    Update tailwind.config.js
    update congif for require

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography';
+import daisyui from 'daisyui';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [typography, daisyui],
   daisyui: {
     themes: ["winter", "dracula"],
   },
